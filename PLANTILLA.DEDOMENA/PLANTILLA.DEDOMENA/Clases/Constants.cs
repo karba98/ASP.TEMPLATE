8443f9278b3bed086ad20cdc5bac1b49e58971da
Karba <raul.castrodlt@outlook.es>
2023-05-06 23:05:20 +0200

    Correcciones + .sln raiz

--- PLANTILLA.DEDOMENA/PLANTILLA.DEDOMENA/Clases/Constants.cs
+++ PLANTILLA.DEDOMENA/PLANTILLA.DEDOMENA/Clases/Constants.cs
@@ -2,15 +2,6 @@
 {
     public static class Constants
     {
-        public const string SEO_tags = "#noticias #seguridad #privada #vigilante " +
-                "#vigilancia #proteccion #alarmas #chaleco seguridad #guantes seguridad" +
-                " #extensible #porra #detectives #escolta# explosivos #cursos #trabajo# " +
-                "ofertas# demanda #trabajador #policia #nacional #municipal #guardia, civil " +
-                "#vigilante seguridad curso #tip #vigilante seguridad #vigilante seguridad empleo " +
-                "#placa vigilante seguridad #certificado profesionalidad vigilante seguridad #vigilante " +
-                "seguridad #agente autoridad #contrato colectivo de trabajo #cuadrante #pistola #puñalada " +
-                "#reconocimiento medico #cacheo #escopeta #union general de trabajadores #declaracion " +
-                "jurada #leon #convocatoria vigilante de seguridad #examen vigilante de seguridad #convenio" +
-                " vigilante seguridad #cuadrante vigilante seguridad #tabla salarial #test vigilante seguridad";
+        public const string SEO_tags = "#noticias #minicho #ofertas...";
     }
 }
